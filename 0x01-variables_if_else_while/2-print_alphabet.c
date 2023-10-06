cbd98d93556671c1aa9c866ebc59e63d1e992032
root <aashrafmohamed84@gmail.com>
2023-10-05 21:23:11 -0700

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	char alpha = 65;
-	for (int i = alpha; i < (alpha+27); i++)
+	char alpha = 97;
+	for (int i = alpha; i < (alpha+26); i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
