5c5f537fc0481545af03678d3b9d52811750343a
root <aashrafmohamed84@gmail.com>
2023-10-10 14:20:38 -0700

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value of a number
+ * @n: the number to computer the modolus for.
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	return (n < 0 ? -n : n);
+}
