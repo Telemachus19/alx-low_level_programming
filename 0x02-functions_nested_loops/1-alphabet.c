a6139c7ec60dde9e85b2e945f1698bdc2e715049
root <aashrafmohamed84@gmail.com>
2023-10-10 13:44:32 -0700

    Udate

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - print all the letters
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char alpha = 'a';
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(alpha + i);
+	}
+	_putchar('\n');
+}	
