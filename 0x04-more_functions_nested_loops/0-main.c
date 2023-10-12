a11f0b1d2d4dd1f7749797e0ffccfd3d2ef75a47
root <aashrafmohamed84@gmail.com>
2023-10-12 13:55:18 -0700

    test file

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stido.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	prinf("%c: %d\n", c, _isupper(c));
+	return (0):
+}
